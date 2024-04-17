4fa3f1e6c530afc78b6010e24811e98844d0ddb4
Ekaterina Chavan <144109471+t0kyolover@users.noreply.github.com>
2024-04-17 21:12:47 +0000

    prep

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Home from "./views/home";
+import Navbar from "./components/navbar";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route element={<h1>Not Found</h1>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
