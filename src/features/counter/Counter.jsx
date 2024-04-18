980bbc2ba0d8bb399a8ebc9812d287af07f441df
Ekaterina Chavan <144109471+t0kyolover@users.noreply.github.com>
2024-04-18 00:51:11 +0000

    counter completed

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice.js";
+import {
+  increment,
+  decrement,
+  selectCount,
+  incrementByAmount,
+} from "./counterSlice.js";
 
 export default function Counter() {
-  const count = useSelector((state) => state.counter.value);
+  const count = useSelector(selectCount);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   return (
     <>
-      <div className="d-lex justify-content-center">
+      <div>
         <h1>Counter</h1>
         <p>Count: {count}</p>
         <button className="me-2" onClick={() => dispatch(increment())}>
@@ -16,6 +22,24 @@ export default function Counter() {
         </button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
       </div>
+      <div className="d-flex flex-row mt-3 align-items-center">
+        <label htmlFor="incrementNumber">Or increment by</label>
+        <div className="ms-1">
+          <input
+            className="rounded w-25"
+            type="number"
+            id="incrementNumber"
+            value={incrementAmount}
+            onChange={(e) => setIncrementAmount(e.target.value)}
+          />
+        </div>
+      </div>
+      <button
+        className="mt-3"
+        onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+      >
+        Increment by amount
+      </button>
     </>
   );
 }
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -19,5 +19,6 @@ export const counterSlice = createSlice({
 });
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const selectCount = (state) => state.counter.value;
 
 export default counterSlice.reducer;
