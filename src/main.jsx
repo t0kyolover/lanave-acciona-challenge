65b85314023e582bf9ce8c06415d1253674d12cb
Ekaterina Chavan <144109471+t0kyolover@users.noreply.github.com>
2024-04-18 14:41:51 +0000

    fixed redux bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import Home from "./views/home";
